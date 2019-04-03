1b73cf36060a8dc170d55a599fb14860d85d9b3d
Lachlan Campbell <lachlan@hackclub.com>
2019-04-03 14:45:29 -0400

    Design updates

--- src/components/Nav.js
+++ src/components/Nav.js
@@ -7,20 +7,26 @@ import Flag from 'components/Flag'
 import Link from 'gatsby-link'
 
 const Base = styled(Box.withComponent('header')).attrs({
-  py: [3, 4],
+  py: 3,
   width: 1
 })`
   z-index: 4;
+  padding-left: 0 !important;
+  padding-right: 0 !important;
 `
 
 const Inner = styled(Container.withComponent(Flex)).attrs({
-  px: 3,
-  maxWidth: 64,
+  maxWidth: 36,
   justify: 'space-between',
   align: 'center',
   mx: 'auto',
   role: 'navigation'
-})``
+})`
+  @supports (padding: calc(max(env(safe-area-inset-left)))) {
+    padding-left: calc(max(${theme.space[2]}px, env(safe-area-inset-left)));
+    padding-right: calc(max(${theme.space[2]}px, env(safe-area-inset-right)));
+  }
+`
 
 const Bar = styled(Box.withComponent('nav'))`
   opacity: 0.875;
@@ -45,26 +51,27 @@ const Item = styled(A.withComponent(Link)).attrs({
 `
 
 const BtnItem = styled(Action).attrs({
-  bg: theme.colors.primary,
+  bg: 'alt',
   fontSize: [2, 3],
   scale: true,
   px: 3,
   py: 2,
   my: 1,
-  mx: 3
+  ml: 3
 })`
+  box-shadow: ${theme.boxShadows[0]} !important;
   text-transform: uppercase;
 `
 
 const Nav = ({ color = 'primary', ...props }) => (
   <Base role="banner" {...props}>
-    <Inner color={color} py={[1, 0]}>
+    <Inner color={color}>
       <Flag />
-      {/* <Bar role="navigation" ml={3} align="left">
-        <Item to="/location" children="Location" />
-        <Item to="/schedule" children="Schedule" />
+      <Bar role="navigation" ml={3} align="left">
+        {/* <Item to="/location" children="Location" />
+        <Item to="/schedule" children="Schedule" /> */}
         <BtnItem to="/register" children="Register" />
-      </Bar> */}
+      </Bar>
     </Inner>
   </Base>
 )
