424ba169381b24ba8752bb68bc87ef6f12e251c2
Lachlan Campbell <lachlan@hackclub.com>
2019-05-20 22:44:24 -0400

    Add Slack + Hearken to sponsors

--- src/components/Sponsors.js
+++ src/components/Sponsors.js
@@ -13,15 +13,17 @@ const Base = styled(Container).attrs({ maxWidth: 72 })`
   ${theme.mediaQueries.md} {
     grid-template-columns: repeat(
       auto-fit,
-      minmax(${props => (props.section === 'sponsors' ? 16 : 14)}rem, 1fr)
+      minmax(${props => (props.section === 'sponsors' ? 21 : 14)}rem, 1fr)
     );
   }
   a {
+    line-height: 0;
     width: 100%;
   }
   img {
+    height: 100%;
     max-width: 75%;
-    max-height: ${props => (props.section === 'sponsors' ? 5 : 3)}rem;
+    max-height: 4rem;
   }
 `
 
@@ -29,7 +31,10 @@ const Sponsors = ({ section = 'sponsors', ...props }) => (
   <Base {...props}>
     {data[section].map(sponsor => (
       <a href={`${sponsor.link}`} target="_blank" key={sponsor.image}>
-        <img alt={sponsor.name} src={`/sponsors/${sponsor.image}`} />
+        <img
+          alt={sponsor.name}
+          src={require(`../../static/sponsors/${sponsor.image}`)}
+        />
       </a>
     ))}
   </Base>
